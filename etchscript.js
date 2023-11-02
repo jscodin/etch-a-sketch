070a6aaa93fa399af26b9370327b937a51b21658
jscodin <jascode9@gmail.com>
2023-11-02 15:01:06 +0000

    test out creation of squares using js and styling using css

--- etchscript.js
+++ etchscript.js
@@ -1,1 +1,11 @@
 // test
+const container = document.querySelector("#container");
+const div1 = document.createElement("div");
+div1.classList.add("sq1");
+container.appendChild(div1);
+const div2 = document.createElement("div");
+div2.classList.add("sq1");
+container.appendChild(div2);
+const div3 = document.createElement("div");
+div3.classList.add("sq1");
+container.appendChild(div3);
