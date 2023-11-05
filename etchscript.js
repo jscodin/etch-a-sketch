660bab6ef1c79c099dde07f81ceaaa371b925f14
jscodin <jascode9@gmail.com>
2023-11-05 21:46:08 +0000

    Update to all files
        - styled button and text on page
        - create logic to allow user to input size of grid
        - amend html for button
        - add checks for user input

--- etchscript.js
+++ etchscript.js
@@ -1,11 +1,47 @@
-// test
-const container = document.querySelector("#container");
-const div1 = document.createElement("div");
-div1.classList.add("sq1");
-container.appendChild(div1);
-const div2 = document.createElement("div");
-div2.classList.add("sq1");
-container.appendChild(div2);
-const div3 = document.createElement("div");
-div3.classList.add("sq1");
-container.appendChild(div3);
+function createGrid() {
+
+  // allow user to input size of grid
+
+  const rows = prompt("Enter no. of rows: (max: 100)");
+  const columns = prompt("Enter no. of columns: (max: 100)");
+
+  // check user input are numbers
+
+  if ((isNaN(rows)) || (isNaN(columns))) {
+    alert("Only positive numbers are accepted");
+  } else {
+    console.log(typeof rows);
+    console.log(typeof columns);
+  }
+
+  // convert output of prompt to numbers
+
+  const rowsInt = parseInt(rows);
+  const columnsInt = parseInt(columns);
+
+  console.log(typeof rowsInt);
+  console.log(typeof columnsInt);
+
+  // check if size of grid is permitted
+
+  if (rowsInt <= 0 || rowsInt > 100 || columnsInt <= 0 || columnsInt > 100) {
+    alert("Size of grid should be between 1 and 100")
+  }
+
+  // create grid logic
+
+
+
+
+
+
+
+
+
+
+}
+
+const btn = document.querySelector(".btn");
+btn.addEventListener('click', createGrid);
+
+
